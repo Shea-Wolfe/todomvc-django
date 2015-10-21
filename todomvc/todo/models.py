faeff201e4923e76c67cdce7ce175ed81710ac32
William Butts <wbutts1023@gmail.com>
2015-10-21 14:45:25 -0400

    Created a simple todo model

--- todomvc/todo/models.py
+++ todomvc/todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class TodoModel(models.Model):
+    title = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+    order models.PositiveSmallIntegerField(null=True, blank=True)
