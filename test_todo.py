337f40d6447ad8221898ec8a888eb8ac68ae0920
William Butts <wbutts1023@gmail.com>
2015-10-22 00:43:48 -0400

    printing out status codes and data to make sure methods are working

--- test_todo.py
+++ test_todo.py
@@ -2,15 +2,27 @@ import requests
 
 
 def post_object():
-    requests.post('http://localhost:8000/api/todos/', data={'title':'This is a test post'})
+    r = requests.post('http://localhost:8000/api/todos/', data={'title':'This is a test post'})
+    print(r.status_code)
+    print(r.text)
 
 post_object()
 
+def get_object():
+    r = requests.get('http://localhost:8000/api/todos/')
+    print(r.status_code)
+    print(r.text)
+get_object()
+
 def delete_object():
-    requests.delete('http://localhost:8000/api/todos/{}'.format(input('please enter the id of the entry to be deleted')))
+    r = requests.delete('http://localhost:8000/api/todos/{}'.format(input('please enter the id of the entry to be deleted')))
+    print(r.status_code)
+    print(r.text)
 
 delete_object()
 
 def put_object():
-    requests.put('http://localhost:8000/api/todos/{}/'.format(input('please enter the id to change')),data={'title':'new_data'})
+    r = requests.put('http://localhost:8000/api/todos/{}/'.format(input('please enter the id to change')),data={'title':'new_data'})
+    print(r.status_code)
+    print(r.text)
 put_object()
