51c0f649431b44707f254b7e7a856f1739db3766
William Butts <wbutts1023@gmail.com>
2015-10-22 00:39:21 -0400

    added a simple put test

--- test_todo.py
+++ test_todo.py
@@ -10,3 +10,7 @@ def delete_object():
     requests.delete('http://localhost:8000/api/todos/{}'.format(input('please enter the id of the entry to be deleted')))
 
 delete_object()
+
+def put_object():
+    requests.put('http://localhost:8000/api/todos/{}/'.format(input('please enter the id to change')),data={'title':'new_data'})
+put_object()
