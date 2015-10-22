b71ac874d83f6790f0f68d8c6b365ccb8eefef9d
William Butts <wbutts1023@gmail.com>
2015-10-22 00:35:13 -0400

    added a functional test for delete

--- test_todo.py
+++ test_todo.py
@@ -5,3 +5,8 @@ def post_object():
     requests.post('http://localhost:8000/api/todos/', data={'title':'This is a test post'})
 
 post_object()
+
+def delete_object():
+    requests.delete('http://localhost:8000/api/todos/{}'.format(input('please enter the id of the entry to be deleted')))
+
+delete_object()
