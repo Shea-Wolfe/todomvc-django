fa10dbba5373bd8e26e5ea5ebe01ff58077044a0
William Butts <wbutts1023@gmail.com>
2015-10-22 00:56:00 -0400

    rewrote tests using nosetests.  all passing!

--- test_todo.py
+++ test_todo.py
@@ -1,28 +1,51 @@
 import requests
 
+#
+# def post_object():
+#     r = requests.post('http://localhost:8000/api/todos/', data={'title':'This is a test post'})
+#     print(r.status_code)
+#     print(r.text)
+#
+# post_object()
+#
+# def get_object():
+#     r = requests.get('http://localhost:8000/api/todos/')
+#     print(r.status_code)
+#     print(r.text)
+# get_object()
+#
+# def delete_object():
+#     r = requests.delete('http://localhost:8000/api/todos/{}'.format(input('please enter the id of the entry to be deleted')))
+#     print(r.status_code)
+#     print(r.text)
+#
+# delete_object()
+#
+# def put_object():
+#     r = requests.put('http://localhost:8000/api/todos/{}/'.format(input('please enter the id to change')),data={'title':'new_data'})
+#     print(r.status_code)
+#     print(r.text)
+# put_object()
 
-def post_object():
+def test_post():
     r = requests.post('http://localhost:8000/api/todos/', data={'title':'This is a test post'})
-    print(r.status_code)
-    print(r.text)
+    assert r.status_code == 201
 
-post_object()
-
-def get_object():
+def test_get():
     r = requests.get('http://localhost:8000/api/todos/')
-    print(r.status_code)
-    print(r.text)
-get_object()
+    assert r.status_code == 200
 
-def delete_object():
-    r = requests.delete('http://localhost:8000/api/todos/{}'.format(input('please enter the id of the entry to be deleted')))
-    print(r.status_code)
-    print(r.text)
+def test_delete():
+    num = requests.get('http://localhost:8000/api/todos/').json()[0]['id']
+    r = requests.delete('http://localhost:8000/api/todos/{}'.format(num))
+    assert r.status_code == 204
 
-delete_object()
-
-def put_object():
-    r = requests.put('http://localhost:8000/api/todos/{}/'.format(input('please enter the id to change')),data={'title':'new_data'})
-    print(r.status_code)
-    print(r.text)
-put_object()
+def test_put():
+    num = requests.get('http://localhost:8000/api/todos/').json()[0]['id']
+    r = requests.put('http://localhost:8000/api/todos/{}/'.format(num),data={'title':'something new'})
+    print(r.json())
+    assert r.status_code == 200
+    assert r.json() == {'title': 'something new',
+                        'id': int('{}'.format(num)),
+                        'order': None,
+                        'completed': False}
